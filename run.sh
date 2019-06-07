19739f5bfe94ed12413e0b52398efe3d040e49af
Cefas Garcia Pereira <cefasgarciapereira@gmail.com>
2019-06-07 02:28:50 -0300

    Repo adjustments

--- run.sh
+++ run.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-gcc sequential-quicksort.c -o sequential-quicksort
-gcc parallel-quicksort.c -o parallel-quicksort -fopenmp
+gcc /src/sequential-quicksort.c -o /src/sequential-quicksort
+gcc /src/parallel-quicksort.c -o /src/parallel-quicksort -fopenmp
 clear
-./sequential-quicksort 10
-./parallel-quicksort 10
-./sequential-quicksort 100
-./parallel-quicksort 100
-./sequential-quicksort 1000
-./parallel-quicksort 1000
-./sequential-quicksort 10000
-./parallel-quicksort 10000
-./sequential-quicksort 100000
-./parallel-quicksort 100000
-./sequential-quicksort 1000000
-./parallel-quicksort 1000000
-./sequential-quicksort 10000000
-./parallel-quicksort 10000000
-./sequential-quicksort 100000000
-./parallel-quicksort 100000000
+./src/sequential-quicksort 10
+./src/parallel-quicksort 10
+./src/sequential-quicksort 100
+./src/parallel-quicksort 100
+# ./src/sequential-quicksort 1000
+# ./src/parallel-quicksort 1000
+# ./src/sequential-quicksort 10000
+# ./src/parallel-quicksort 10000
+# ./src/sequential-quicksort 100000
+# ./src/parallel-quicksort 100000
+# ./src/sequential-quicksort 1000000
+# ./src/parallel-quicksort 1000000
+# ./src/sequential-quicksort 10000000
+# ./src/parallel-quicksort 10000000
+# ./src/sequential-quicksort 100000000
+# ./src/parallel-quicksort 100000000
